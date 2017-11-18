da7d6a75697cf38cea5dadf5126a811af4e0a5b3
Jakub Tesárek <mail@jakubtesarek.cz>
2017-11-18 18:19:24 +0100

    flash messages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  init_flash_messages();
+  init_form();
+  init_external_links();
 });
 
 
@@ -10,19 +11,46 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-56VWP8D');
 
-// EXTERNAL LINKS
-$('a.external' ).attr('target', '_blank');
-
-// FLASH MESSAGES
-function init_flash_messages() {
-  $('body').on('click', '.flash > button', function(event){
-    $(this).parent('.flash').remove()
-  })
-}
-function flash_failure(message) {
-  $('#flashes').append('<div class="flash failure"><p>' + message + '</p><button type="button">&times;</button></div>');
+function init_external_links() {
+	$('a.external' ).attr('target', '_blank');	
 }
 
-function flash_success(message) {
-  $('#flashes').append('<div class="flash success"><p>' + message + '</p><button type="button">&times;</button></div>');
+function init_form() {
+    $('body').on('click', '.flash > button', function(event){
+      $(this).parent('.flash').remove()
+    })
+
+    $('#contact-form').submit(function(e) {
+      var elem_button = $(this).find('input[type="submit"]');
+      var send_text = elem_button.val()
+      var elem_message = $(this).find('[name="message"]');
+      var elem_replyto = $(this).find('[name="_replyto"]');
+      var elem_subject = $(this).find('[name="_subject"]');
+
+      elem_button.prop('disabled', true);
+      elem_button.val('Odesílám ...')
+      $.ajax({
+        type: 'POST',
+        url: $(this).attr('action'),
+        dataType: 'json',
+        data: {
+          message: elem_message.val(),
+          _replyto: elem_replyto.val(),
+          _subject: elem_subject.val()
+        },
+        success: function(data) {
+          $('#flashes').append('<div class="flash success"><p>Děkuji, na vaši zprávu odpovíme co nejdříve.</p><button type="button">&times;</button></div>');
+          elem_message.val('');
+          elem_replyto.val('');
+        },
+        error: function(data) {
+          $('#flashes').append('<div class="flash failure"><p>Při zpracování formuláře bohužel došlo k chybě. Zkuste nám prosím napsat přímo na  <a href="mailto:katka@barevneuceni.cz?subject=' + elem_subject.val() + '&body=' + elem_message.val() + '">katka@barevneuceni.cz</a>.</p><button type="button">&times;</button></div>');
+          console.log('fail');
+        }
+      });
+      elem_button.val(send_text)
+      elem_button.prop('disabled', false);
+      e.preventDefault();
+  });
 }
+
